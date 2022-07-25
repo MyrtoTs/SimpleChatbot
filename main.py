3843d74dff83e220a7874e3f97520c48b8c155c9
MyrtoTs <mtsokanaridou@gmail.com>
2022-07-25 21:25:37 +0300

    words tokenized & stemmed.

--- main.py
+++ main.py
@@ -14,10 +14,7 @@ with open('intents.json') as file:
     data = json.load(file)
 
 # initializations
-words = []
-labels = []
-docs_x = []
-docs_y = []
+words, labels, docs_x, docs_y = [], [], [], []
 
 for intent in data['intents']:
     for pattern in intent['patterns']:
@@ -31,3 +28,18 @@ for intent in data['intents']:
 
     if intent['tag'] not in labels:
         labels.append(intent['tag'])
+
+# print(f"\nDoc_x now are: \n {docs_x} \n\n while docs_y are: \n {docs_y} \n\n and labels are: \n {labels}")
+
+# Stemming
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+print(f"\nStemmed words are: \n {words}\n" )
+
+words = sorted(list(set(words)))
+    # set => no duplicates
+    # list => same type as before
+    # sorted => on alphabetical order
+
+print(f"\n and after sort: \n {words}\n" )
+
+labels = sorted(labels)
