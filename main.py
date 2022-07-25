60357c3bdd027e821b5e5586ef10b01961675f62
MyrtoTs <mtsokanaridou@gmail.com>
2022-07-25 22:20:17 +0300

    bag of words via vectorizer.

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import nltk
-from nltk.stem.lancaster import LancasterStemmer
-
-stemmer = LancasterStemmer()
+from sklearn.feature_extraction.text import CountVectorizer
 
 import numpy
 import tflearn
@@ -18,28 +16,16 @@ words, labels, docs_x, docs_y = [], [], [], []
 
 for intent in data['intents']:
     for pattern in intent['patterns']:
-        wrds = nltk.word_tokenize(pattern)
-        # Are you cash only?
-        # Tokenize:['Are', 'you', 'cash', 'only', '?']
-        # print(f"Words now are {words} \n while docs_x are {docs_x}\n\n\n")
-        words.extend(wrds)
-        docs_x.append(wrds)
+        docs_x.append(pattern)
         docs_y.append(intent["tag"])
 
     if intent['tag'] not in labels:
         labels.append(intent['tag'])
 
-# print(f"\nDoc_x now are: \n {docs_x} \n\n while docs_y are: \n {docs_y} \n\n and labels are: \n {labels}")
+print(f"\nDoc_x now are: \n {docs_x} \n\n while docs_y are: \n {docs_y} \n\n and labels are: \n {labels}")
 
-# Stemming
-words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-print(f"\nStemmed words are: \n {words}\n" )
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(docs_x)
+bag_of_words = X.toarray()
 
-words = sorted(list(set(words)))
-    # set => no duplicates
-    # list => same type as before
-    # sorted => on alphabetical order
-
-print(f"\n and after sort: \n {words}\n" )
-
-labels = sorted(labels)
+print(f"\n Bag of words array : \n {bag_of_words}")
