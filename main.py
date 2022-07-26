22b421e674a1bcc08ce2296852c25013b06a15fe
MyrtoTs <mtsokanaridou@gmail.com>
2022-07-26 15:30:43 +0300

    full code

--- main.py
+++ main.py
@@ -73,3 +73,57 @@ bag_of_words_output = numpy.array(output)
 #     f.write(f"bag of words training array of type {type(bag_of_words)} of size {bag_of_words.shape} : \n {bag_of_words}\n")
 #     f.write(f"bag of words output array of size {type(bag_of_words_output)}: \n {bag_of_words_output}\n")
 
+# Create NN model
+
+tensorflow.compat.v1.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+# Train model or load pretrained model
+
+try:
+    model.load("model/model.tflearn")
+except:
+    model.fit(bag_of_words, bag_of_words_output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model/model.tflearn")
+
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return numpy.array(bag)
+
+
+def chat():
+    print("Start talking with the bot (type quit to stop)!")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+
+        results = model.predict([bag_of_words(inp, words)])
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+
+        print(random.choice(responses))
+
+
+chat()
